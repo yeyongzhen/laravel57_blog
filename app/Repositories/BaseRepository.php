31023b6a8b515d667d1734db5bea2603b28fb34e
yeyongzhen <706528598@qq.com>
2019-02-24 16:14:03 +0800

    新建自动出创建Repository类文件的artisan命令

---
+++ app/Repositories/BaseRepository.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Repositories;
+
+class BaseRepository
+{
+    /**
+     * @return mixed
+     */
+    public function getAll()
+    {
+        return $this->query()->get();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCount()
+    {
+        return $this->query()->count();
+    }
+
+    /**
+     * @param $id
+     *
+     * @return mixed
+     */
+    public function find($id)
+    {
+        return $this->query()->find($id);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function query()
+    {
+        return call_user_func(static::MODEL.'::query');
+    }
+}
